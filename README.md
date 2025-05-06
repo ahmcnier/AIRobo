ac612e1545992216c9454cef154a512842ab0d94
ahmcnier <amcnier8@gmail.com>
2025-05-06 22:12:24 +0100

    loads in test and train datasets. Evaluates an image based on the YOLO pretrained model successfully

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ First AI Robotics Project
 I aim to make a pet teasing robot toy. The robot should be able to chase the pet and/or run away from the pet. 
 
 I will have combine object detection models and deep learning to detect the pet. An ultrasound sensor will be fitted to detect proximity of the pet. 
+
+I have used a dataset consisting of dog and cat pictures (sourced from https://github.com/laxmimerit/dog-cat-full-dataset.git), in an attempt to reduce overfitting the model towards one specific animal or type of animal. 
--- src/main/run.py
+++ src/main/run.py
@@ -1,17 +1,28 @@
 import os
 import torch
+from PIL import Image
+from ultralytics import YOLO
 
 print("CUDA Available:", torch.cuda.is_available())
 print("CUDA Version:", torch.version.cuda)
 print("GPU:", torch.cuda.get_device_name(0))
 
-def main():
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
 
-    test_path = os.path.join(base_dir, 'test')
-    train_path = os.path.join(base_dir, 'train')
+test_path = os.path.join(base_dir, 'test')
+train_path = os.path.join(base_dir, 'train')
 
-    test_cat_path = os.path.join(test_path, 'cat')
-    test_dog_path = os.path.join(test_path, 'dog')
-    train_cat_path = os.path.join(train_path, 'cat')
-    train_dog_path = os.path.join(train_dog_path, 'dog')
+test_cat_path = os.path.join(test_path, 'cats')
+test_dog_path = os.path.join(test_path, 'dogs')
+train_cat_path = os.path.join(train_path, 'cats')
+train_dog_path = os.path.join(train_path, 'dogs')
+
+test_image_files = [f for f in os.listdir(test_cat_path)]
+test_image_files.extend([f for f in os.listdir(test_dog_path)])
+
+train_image_files = [f for f in os.listdir(train_cat_path)]
+train_image_files.extend([f for f in os.listdir(train_dog_path)])
+
+model = YOLO('yolov8s.pt')
+results = model(os.path.join(test_cat_path, test_image_files[0]))
+results[0].show()
